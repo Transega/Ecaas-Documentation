022ffe65f50f7e0ba8bae21ceba556d0a352bc22
Transega <collinsasega.ca@gmail.com>
2022-06-15 18:25:11 +0300

    hide edit on github

--- conf.py
+++ conf.py
@@ -68,7 +68,7 @@ html_context = {
     "commit": False,
 }
 
-html_show_sourcelink = False
+# html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 html_show_sphinx = False
@@ -77,3 +77,7 @@ html_show_sphinx = False
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_css_files = [
+    'css/custom.css',
+]
